12e29e0f640f540991cbe439590faac2169b696c
smack0007 <zachary.snow@gmail.com>
2022-11-03 16:07:56 +0100

    Implemented lots of methods from Result.

--- src/result.test.ts
+++ src/result.test.ts
@@ -1,13 +1,14 @@
+import { assertThrows } from "https://deno.land/std@0.159.0/testing/asserts.ts";
 import { asserts, bdd } from "../deps.ts";
 const { assertEquals } = asserts;
 const { beforeEach, describe, it } = bdd;
-import { error, ErrorResult, ok, OkResult } from "./result.ts";
+import { error, ErrorResult, ok, OkResult, Result } from "./result.ts";
 import { Equal, Expect } from "./tests.ts";
 
-type _tests = [
-	Expect<Equal<OkResult<number>["ok"], true>>,
+type TypeTests = [
+	Expect<Equal<OkResult<number>["isOk"], true>>,
 
-	Expect<Equal<ErrorResult<number>["ok"], false>>,
+	Expect<Equal<ErrorResult<number>["isOk"], false>>,
 ];
 
 describe("Result", () => {
@@ -18,16 +19,16 @@ describe("Result", () => {
 			result = ok(42);
 		});
 
-		it("should set ok to true", () => {
-			assertEquals(result.ok, true);
+		it("should set isOk to true", () => {
+			assertEquals(result.isOk, true);
+		});
+
+		it("should set isError to false", () => {
+			assertEquals(result.isError, false);
 		});
 
 		it("should set result to 42", () => {
 			assertEquals(result.value, 42);
-		});
-
-		it("should unwrap result", () => {
-			assertEquals(result.unwrap((x) => x.toString()), "42");
 		});
 	});
 
@@ -39,15 +40,222 @@ describe("Result", () => {
 		});
 
 		it("should set ok to false", () => {
-			assertEquals(result.ok, false);
+			assertEquals(result.isOk, false);
+		});
+
+		it("should set isError to true", () => {
+			assertEquals(result.isError, true);
 		});
 
 		it("should set error to 42", () => {
 			assertEquals(result.error, 42);
 		});
+	});
+
+	describe("and", () => {
+		it("should take other if isOk is true", () => {
+			const result1 = ok(42);
+			const result2 = ok(12);
+
+			assertEquals(result1.and(result2), result2);
+
+			const result3 = ok(42);
+			const result4 = error(12);
+
+			assertEquals(result3.and(result4), result4);
+		});
+
+		it("should take self if isOk is false", () => {
+			const result1 = error(42);
+			const result2 = ok(12);
+
+			assertEquals(result1.and(result2), result1);
+
+			const result3 = error(42);
+			const result4 = error(12);
+
+			assertEquals(result3.and(result4), result3);
+		});
+	});
+
+	describe("isOkAnd", () => {
+		it("should call predicate if isOk is true", () => {
+			const result = ok(42);
+
+			let predicateCalled = false;
+			const isOkAndResult = result.isOkAnd((value) => {
+				predicateCalled = true;
+				return value >= 42;
+			});
+
+			assertEquals(predicateCalled, true);
+			assertEquals(isOkAndResult, true);
+		});
+
+		it("should not call predicate if isOk is false", () => {
+			const result = error(42);
+
+			let predicateCalled = false;
+			const isOkAndResult = result.isOkAnd((_) => {
+				predicateCalled = true;
+				return false;
+			});
+
+			assertEquals(predicateCalled, false);
+			assertEquals(isOkAndResult, false);
+		});
+	});
+
+	describe("isErrorAnd", () => {
+		it("should call predicate if isError is true", () => {
+			const result = error(42);
+
+			let predicateCalled = false;
+			const isErrorAndResult = result.isErrorAnd((value) => {
+				predicateCalled = true;
+				return value >= 42;
+			});
+
+			assertEquals(predicateCalled, true);
+			assertEquals(isErrorAndResult, true);
+		});
+
+		it("should not call predicate if isError is false", () => {
+			const result = ok(42);
+
+			let predicateCalled = false;
+			const isErrorAndResult = result.isErrorAnd((_) => {
+				predicateCalled = true;
+				return false;
+			});
+
+			assertEquals(predicateCalled, false);
+			assertEquals(isErrorAndResult, false);
+		});
+	});
+
+	describe("map", () => {
+		it("should map if isOk is true", () => {
+			const result = ok(42);
+			const result2 = result.map((x) => x.toString());
+
+			assertEquals(result2.isOk, true);
+			assertEquals(result2.unwrap(), "42");
+		});
+
+		it("should not map if isOk is false", () => {
+			const result = error(42);
+			const result2 = result.map((x) => (x as Result<number, number>).toString());
+
+			assertEquals(result2.isOk, false);
+			assertThrows(() => {
+				result2.unwrap();
+			});
+		});
+	});
+
+	describe("mapError", () => {
+		it("should map if isOk is false", () => {
+			const result = error(42);
+			const result2 = result.mapError((x) => x.toString());
+
+			assertEquals(result2.isOk, false);
+			assertEquals(result2.unwrapError(), "42");
+		});
+
+		it("should not map if isOk is true", () => {
+			const result = ok(42);
+			const result2 = result.mapError((x) => (x as Result<number, number>).toString());
+
+			assertEquals(result2.isOk, true);
+			assertThrows(() => {
+				result2.unwrapError();
+			});
+		});
+	});
+
+	describe("or", () => {
+		it("should take self if isOk is true", () => {
+			const result1 = ok(42);
+			const result2 = ok(12);
+
+			assertEquals(result1.or(result2), result1);
+
+			const result3 = ok(42);
+			const result4 = error(12);
+
+			assertEquals(result3.or(result4), result3);
+		});
+
+		it("should take other if isOk is false", () => {
+			const result1 = error(42);
+			const result2 = ok(12);
+
+			assertEquals(result1.or(result2), result2);
+
+			const result3 = error(42);
+			const result4 = error(12);
+
+			assertEquals(result3.or(result4), result4);
+		});
+	});
+
+	describe("unwrap", () => {
+		it("should unwrap result", () => {
+			const result = ok(42);
+			assertEquals(result.unwrap(), 42);
+			assertEquals(result.unwrap((x) => x.toString()), "42");
+		});
+
+		it("should throw if error callback not specified", () => {
+			const result = error(42);
+			assertThrows(() => {
+				result.unwrap((x) => x);
+			});
+		});
 
 		it("should unwrap error", () => {
-			assertEquals(result.unwrap((_) => "Not 42", (x) => x?.toString()), "42");
+			const result = error(42);
+			assertEquals(result.unwrap(() => "Not 42", (x) => x.toString()), "42");
+		});
+	});
+
+	describe("unwrapError", () => {
+		it("should unwrap error", () => {
+			const result = error(42);
+			assertEquals(result.unwrapError(), 42);
+			assertEquals(result.unwrapError((x) => x.toString()), "42");
+		});
+
+		it("should throw if not error", () => {
+			const result = ok(42);
+			assertThrows(() => {
+				result.unwrapError((x) => x);
+			});
+		});
+	});
+
+	describe("unwrapOr", () => {
+		it("should unwrap result", () => {
+			const result = ok(42);
+			assertEquals(result.unwrapOr(12), 42);
+		});
+
+		it("should take default for error", () => {
+			const result = error(42);
+			assertEquals(result.unwrapOr(12), 12);
+		});
+	});
+
+	describe("unwrapOrElse", () => {
+		it("should unwrap result", () => {
+			const result = ok(42);
+			assertEquals(result.unwrapOrElse(() => 12), 42);
+		});
+
+		it("should call callback for error", () => {
+			const result = error(42);
+			assertEquals(result.unwrapOrElse(() => 12), 12);
 		});
 	});
 });
--- src/result.ts
+++ src/result.ts
@@ -1,12 +1,43 @@
+type Predicate<T> = (value: T) => boolean;
+
 interface ResultMethods<T, E> {
-	unwrap<U, F>(
-		ok: (value: T) => U,
-		error?: (error?: E) => F,
-	): U | F;
+	and(other: Result<T, E>): Result<T, E>;
+
+	isOkAnd(predicate: Predicate<T>): boolean;
+
+	isErrorAnd(predicate: Predicate<E>): boolean;
+
+	map<U>(callback: (value: T) => U): Result<U, E>;
+
+	mapError<F>(callback: (error: E) => F): Result<T, F>;
+
+	or(other: Result<T, E>): Result<T, E>;
+
+	unwrap(): T;
+	unwrap<U>(ok: (value: T) => U): U;
+	unwrap<U, F>(ok: (value: T) => U, error: (error: E) => F): U | F;
+
+	unwrapError(): E;
+	unwrapError<F>(error: (error: E) => F): F;
+
+	unwrapOr(defaultValue: T): T;
+
+	unwrapOrElse(callback: () => T): T;
 }
 
-export type OkResult<T> = { ok: true; value: T } & ResultMethods<T, never>;
-export type ErrorResult<E> = { ok: false; error?: E } & ResultMethods<never, E>;
+interface ResultProperties<T, E> {
+	readonly isError: boolean;
+}
+
+export type OkResult<T> =
+	& { readonly isOk: true; value: T }
+	& ResultMethods<T, unknown>
+	& ResultProperties<T, unknown>;
+
+export type ErrorResult<E> =
+	& { readonly isOk: false; error: E }
+	& ResultMethods<unknown, E>
+	& ResultProperties<unknown, E>;
 
 export type Result<T, E = unknown> =
 	| OkResult<T>
@@ -25,26 +56,101 @@ function throwError<E>(message: string, error?: E): never {
 	throw new ResultError(message, error);
 }
 
-function unwrap<T, E extends Error, U, F>(
-	this: Result<T, E>,
-	ok: (value: T) => U,
-	error?: (error?: E) => F,
-): U | F {
-	return this.ok ? ok(this.value) : (error ? error(this.error) : throwError("Failed to unwrap result.", this.error));
+const methods = {
+	and: function <T, E>(
+		this: Result<T, E>,
+		other: Result<T, E>,
+	): Result<T, E> {
+		return this.isOk ? other : this;
+	},
+
+	isOkAnd: function <T, E>(
+		this: Result<T, E>,
+		predicate: Predicate<T>,
+	): boolean {
+		return this.isOk && predicate(this.value);
+	},
+
+	isErrorAnd: function <T, E>(
+		this: Result<T, E>,
+		predicate: Predicate<E>,
+	): boolean {
+		return this.isError && predicate((this as ErrorResult<E>).error);
+	},
+
+	map: function <T, E, U>(
+		this: Result<T, E>,
+		callback: (value: T) => U,
+	): Result<U, E> {
+		return this.isOk ? ok(callback(this.value)) : this;
+	},
+
+	mapError: function <T, E, F>(
+		this: Result<T, E>,
+		callback: (value: E) => F,
+	): Result<T, F> {
+		return !this.isOk ? error(callback(this.error)) : this;
+	},
+
+	or: function <T, E>(
+		this: Result<T, E>,
+		other: Result<T, E>,
+	): Result<T, E> {
+		return this.isOk ? this : other;
+	},
+
+	unwrap: function <T, E, U, F>(
+		this: Result<T, E>,
+		ok?: (value: T) => U,
+		error?: (error: E) => F,
+	): T | U | F {
+		return this.isOk
+			? (ok ? ok(this.value) : this.value)
+			: (error ? error(this.error) : throwError("Failed to unwrap value.", this.error));
+	},
+
+	unwrapError: function <T, E, F>(
+		this: Result<T, E>,
+		error?: (error: E) => F,
+	): E | F {
+		return !this.isOk ? (error ? error(this.error) : this.error) : throwError("Failed to unwrap error.", this.value);
+	},
+
+	unwrapOr: function <T, E>(
+		this: Result<T, E>,
+		defaultValue: T,
+	): T {
+		return this.isOk ? this.value : defaultValue;
+	},
+
+	unwrapOrElse: function <T, E>(
+		this: Result<T, E>,
+		callback: () => T,
+	): T {
+		return this.isOk ? this.value : callback();
+	},
+};
+
+const properties: PropertyDescriptorMap = {
+	isError: {
+		get: function (this: Result<unknown, unknown>): boolean {
+			return !this.isOk;
+		},
+	},
+};
+
+export function ok<T>(value: T): OkResult<T> {
+	return Object.create({
+		isOk: true,
+		value,
+		...(methods as ResultMethods<T, unknown>),
+	}, properties);
 }
 
-export function ok<T>(data: T): OkResult<T> {
-	return {
-		ok: true,
-		value: data,
-		unwrap: unwrap as ResultMethods<T, never>["unwrap"],
-	};
-}
-
-export function error<E>(error?: E): ErrorResult<E> {
-	return {
-		ok: false,
+export function error<E>(error: E): ErrorResult<E> {
+	return Object.create({
+		isOk: false,
 		error,
-		unwrap: unwrap as ResultMethods<never, E>["unwrap"],
-	};
+		...(methods as ResultMethods<unknown, E>),
+	}, properties);
 }
